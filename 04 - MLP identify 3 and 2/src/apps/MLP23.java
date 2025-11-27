848f6341cfed5f330c8b9a17afa6f63e3dfdd14b
Brandon Mejia <129073713+kanekitakitos@users.noreply.github.com>
2025-11-27 00:57:36 +0000

    update

--- 04 - MLP identify 3 and 2/src/apps/MLP23.java
+++ 04 - MLP identify 3 and 2/src/apps/MLP23.java
@@ -15,10 +15,10 @@ import neural.MLP;
 
 public class MLP23 {
 
-    private double lr = 0.004;
+    private double lr = 0.0015;
 
-    private int epochs = 5000;
-    private int[] topology = {400,2, 1};
+    private int epochs = 15000;
+    private int[] topology = {400,1, 1};
     private IDifferentiableFunction[] functions = {new Sigmoid(),new Sigmoid(), new Sigmoid()};
     private MLP mlp;
     private static int seek = 4;
